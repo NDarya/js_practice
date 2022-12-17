78878b4d7b5c8ed731f23b765b47ed403b570452
DaryaN <daryanotchenko@gmail.com>
2022-12-17 14:26:13 +0500

    changes

--- script.js
+++ script.js
@@ -2,22 +2,25 @@ let name = 'Alex'
 let money = 10000
 let account = 7777
 let user = prompt("Как вас зовут?")
+let bill = prompt('Номер счета?')
+let ask = prompt('Сколько обналичить?')
 
-
-
-if(user === name){
-    alert('Welcome')
-    if (prompt('Номер счета?') === account){
-    alert('Identified')} else{
-        alert('Пользователь не найден, досвидули')
-    }
-    if(prompt('Сколько обналичить?')<= money){
+if (user === name) {
+   
+    if (+bill === account) {
+        
+        if (ask <= money) {
         alert('Все отлично')
-    } else(
-        alert('Недостаточно средств')
-    )
-} else{
-    alert('Пользователь не найден, досвидули')
+        console.log(`Осталось на счету: ${money - ask}`);
+        console.log(`Снято: ${ask}`);
+        } else {
+            alert('Недостаточно средств')
+        }
+    } else {
+        alert('Пользователь не найден')
+    }
+} else {
+    alert('Пользователь не найден')
 }
 
 
@@ -28,18 +31,18 @@ let age = prompt('How old are you?')
 
 
 
-if(userName[0]=== 'A' || userName[0]=== 'a'){
-    alert('Welcome')
+if (userName[0] === 'A' || userName[0] === 'a') {
+    
+    if (age >= 18 && age <= 40) {
+    
+        if (prompt('How much $ can you invest?') >= 100) {
+            alert('Welcome')
+        } else {
+            alert('Sorry')
+            }
+    } else {
+        alert('Sorry')
+    }
 } else {
-    alert('You are not invited')
-}
-if(age >= 18 && age <=40){
-    alert('That is great!')
-} else{
-    alert('Sorry :(')
-}
-if(prompt('Do you have 100$?') === true){
-    alert('Completely pleased to have you here!')
-} else{
-    alert('Sorry, go home')
+    alert('Sorry')
 }
